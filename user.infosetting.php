544f40160bcbc87ffad40f89c3089dc16000bca7
Maulana Alirridlo <59948773+MaulanaAlirridlo@users.noreply.github.com>
2020-12-06 15:34:18 +0700

    Update user-infosetting

--- user.infosetting.php
+++ user.infosetting.php
@@ -9,7 +9,7 @@ include './include/script.php';
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" href="./assets/css/adduser.css">
+  <link rel="stylesheet" href="./assets/css/userinfosetting.css">
   <link rel="stylesheet" href="./assets/css/userlist.css">
   <link rel="stylesheet" href="./assets/css/navbar.css">
 </head>
@@ -22,16 +22,21 @@ include './include/script.php';
     </div>
     <div class="col">
       <form action="/action_page.php" class="form-group">
-      <input class="form-control" type="text" id="fnama" name="nama" placeholder="Your full name..">
+      <div class="row p-3">
+        <div class="photo-profile">
+          <img 
+          src="https://sps.widyatama.ac.id/wp-content/uploads/2020/08/dummy-profile-pic-male1-300x300.jpg"
+          alt="profile">
+        <input type="file" name="foto" id="foto" class="pl-2">
+        </div>
+      </div>
       <div class="row mt-2">
           <div class="col-2">
             <label for="fnama">Nama</label>
           </div>
-          <div class="col-75">
-            <input class="form-control" type="text" id="fnama" name="nama" placeholder="Your first name..">
-            <div class="col-75">
-            <input class="form-control" type="text" id="fnama" name="nama" placeholder="Your last name..">
-          </div>
+          <div class="col-75 d-flex">
+            <input class="form-control w-50 nama-depan" type="text" id="nama-depan" name="nama-depan" placeholder="Nama depan">         
+            <input class="form-control w-50" type="text" id="nama-belakang" name="nama-belakang" placeholder="Nama belakang">
          </div>
         </div>
         <div class="row mt-2">
@@ -39,7 +44,7 @@ include './include/script.php';
             <label for="fnohp">No HP</label>
           </div>
           <div class="col-75">
-            <input class="form-control" type="number" id="fnohp" name="nohp" placeholder="Your number hanphone..">
+            <input class="form-control" type="number" id="fnohp" name="nohp" placeholder="No hp">
           </div>
         </div>
         <div class="row mt-2">
@@ -47,7 +52,7 @@ include './include/script.php';
             <label for="fwa">WhatsApp</label>
           </div>
           <div class="col-75">
-            <input class="form-control" type="number" id="fwa" name="nowa" placeholder="Your number whatsapp ..">
+            <input class="form-control" type="number" id="fwa" name="nowa" placeholder="No wa">
           </div>
         </div>
         <div class="row">
@@ -55,7 +60,7 @@ include './include/script.php';
             <label for="Alamat">Alamat</label>
           </div>
           <div class="col-75">
-            <input class="form-control" type="text" id="Alamat" name="Alamat" placeholder="Your Alamat..">
+            <textarea name="alamat" id="alamat" class="form-control" placeholder="Alamat""></textarea>
           </div>
         </div>
         <div class="row">
@@ -63,13 +68,16 @@ include './include/script.php';
             <label for="Deskripsi">Deskripsi</label>
           </div>
           <div class="col-75">
-            <input class="form-control" type="text" id="Deskripsi" name="Deskripsi" placeholder="Your Deskripsi..">
+            <textarea name="alamat" id="deskripsi" class="form-control" placeholder="Deskripsi anda""></textarea>
           </div>
         </div>
 
 
-        <div class="pl-3 row">
-          <input type="submit" value="Simpan">
+        <div class="pl-3 row py-3">
+          <div class="col-2"></div>
+          <div class="col-75">
+            <input type="submit" value="Simpan" class="ml-auto">
+          </div>
         </div>
       </form>
     </div>
