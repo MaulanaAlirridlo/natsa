066d18ad6b22af6a5189809e594190ea22442aa1
anjualiciajohan <anjujohan0@gmail.com>
2020-12-06 14:12:52 +0700

    update user.infosetting.php

--- user.infosetting.php
+++ user.infosetting.php
@@ -17,20 +17,32 @@
     </div>
     <div class="col">
       <form action="/action_page.php" class="form-group">
+      <input class="form-control" type="text" id="fnama" name="nama" placeholder="Your full name..">
+      <div class="row mt-2">
+          <div class="col-2">
+            <label for="fnama">Nama</label>
+          </div>
+          <div class="col-75">
+            <input class="form-control" type="text" id="fnama" name="nama" placeholder="Your first name..">
+            <div class="col-75">
+            <input class="form-control" type="text" id="fnama" name="nama" placeholder="Your last name..">
+          </div>
+         </div>
+        </div>
         <div class="row mt-2">
           <div class="col-2">
-            <label for="fname">First Name</label>
+            <label for="fnohp">No HP</label>
           </div>
           <div class="col-75">
-            <input class="form-control" type="text" id="fname" name="firstname" placeholder="Your name..">
+            <input class="form-control" type="number" id="fnohp" name="nohp" placeholder="Your number hanphone..">
           </div>
         </div>
-        <div class="row">
+        <div class="row mt-2">
           <div class="col-2">
-            <label for="lname">Last Name</label>
+            <label for="fwa">WhatsApp</label>
           </div>
           <div class="col-75">
-            <input class="form-control" type="text" id="lname" name="lastname" placeholder="Your last name..">
+            <input class="form-control" type="number" id="fwa" name="nowa" placeholder="Your number whatsapp ..">
           </div>
         </div>
         <div class="row">
@@ -52,7 +64,7 @@
 
 
         <div class="pl-3 row">
-          <input type="submit" value="Submit">
+          <input type="submit" value="Simpan">
         </div>
       </form>
     </div>
