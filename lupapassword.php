b9f9d98c3c1deb1b76e562924843846b5b77d9ae
anjualiciajohan <anjujohan0@gmail.com>
2020-12-16 12:25:01 +0700

    lupa_passwprd

---
+++ lupapassword.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>lupa password</title>
+        <link rel="stylesheet" href="lupapassword.css">
+    </head>
+   
+    <body>
+        <div class="container">
+          <h1>Lupa password</h1>
+            <form>
+                <label>Email</label><br>
+                <input type="email"><br>
+                
+                <button>Submit</button>
+            </form>
+        </div>     
+    </body>
+</html>
