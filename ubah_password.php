3899d772bbe262a5547322fc386590b51e1c9184
anjualiciajohan <anjujohan0@gmail.com>
2020-12-19 00:37:00 +0700

    add ubah_password

---
+++ ubah_password.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Ubah password</title>
+		<link rel="stylesheet" href="assets/css/ubah_password.css">
+    </head>
+   
+    <body>
+        <div class="container">
+          <h1>Ubah password</h1>
+            <form>
+           
+                <input type="password" placeholder="Password baru"><br>
+                <p>
+                <input type="password" placeholder="ketik password baru kembali "><br>
+                <p>
+                <button>Submit</button>
+            </form>
+        </div>     
+    </body>
+</html>
