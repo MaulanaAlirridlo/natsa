295293531caffe4b8b9073425fbd68eefd63cb3a
Naufal Farros <naufalfarros05@gmail.com>
2021-01-03 17:38:36 +0700

    center button load more

--- index.php
+++ index.php
@@ -34,11 +34,12 @@ include './include/script.php';
     </div>
   </div>
 
-  <div class="row">
-    <div class="col-1"></div>
-    <div class="col-2 p-0"></div>
-    <div class="col">
-        <button class="btn btn-primary" id="loadMore" current="">LOAD MORE</button>
+  <div class="row align-items-center mt-2 mb-2">
+    <div class="col-6 mr-3"></div>
+    <div class="col pl-5">
+      <button class="btn btn-primary font-weight-bold" id="loadMore" current="">LOAD MORE</button>
+    </div>
+    <div style="col">
     </div>
   </div>
 
